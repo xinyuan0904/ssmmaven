2c7fd447a93d624146b90cf01d4361886822bf6b
xinyuan0904@163.com <xinyuan0904@163.com>
2017-05-16 15:13:47 +0800

    add angular.js update charset

--- src/main/java/pers/yuan/maven/controller/UserController.java
+++ src/main/java/pers/yuan/maven/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
 
 import pers.yuan.maven.pojo.User;
 import pers.yuan.maven.service.IUserService;
@@ -25,11 +28,17 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 	
+	//@RequestMapping(value="/getUserList",produces = "text/html;charset=UTF-8") //中文乱码
 	@RequestMapping("/getUserList")
+	@ResponseBody
 	public String getUserList(Model model){
+		JSONObject jo = new JSONObject();
 		List<User> list = this.userService.getUserList();
-		model.addAttribute("ulist",list);
-		return "user";
+		jo.put("code", 200);
+		jo.put("users", list);
+//		model.addAttribute("ulist",list);
+//		return "user";
+		return jo.toString();
 	}
 	
 }
